1cd970d11790517fe3bf08d453dc6fcf19742b79
Natalia_Konovalenko <natalia_konovalenko@epam.com>
2020-01-15 13:08:42 +0200

    onClick, ref

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -4,10 +4,16 @@ import DialogItem from "./DialogItem/DialogItem";
 import Message from "./Message/Message";
 
 const Dialogs = (props) => {
-    console.log(props);
 
     let dialogsElements = props.data.dialogs.map( dialog => <DialogItem name={dialog.name} id={dialog.id} />);
     let messagesElements = props.data.messages.map( el => <Message message={el.message} />);
+
+    let newMessageElement = React.createRef();
+
+    let addMessage = () => {
+        let text = newMessageElement.current.value;
+        alert(text);
+    };
 
     return (
         <div className={s.dialogs}>
@@ -16,6 +22,12 @@ const Dialogs = (props) => {
             </div>
             <div className={s.messages}>
                 { messagesElements }
+                <div>
+                    <textarea ref={newMessageElement}></textarea>
+                </div>
+                <div>
+                    <button onClick={addMessage}>Add message</button>
+                </div>
             </div>
         </div>
     )
--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -4,19 +4,24 @@ import Post from './Post/Post';
 
 const MyPosts  = (props) => {
 
-    console.log('myPosts', props);
-
     let postsElements = props.posts.map( el => <Post message={el.message} like={el.likesCount} /> );
+
+    let newPostElement = React.createRef();
+
+    let addPost = () => {
+        let text = newPostElement.current.value;
+        alert(text);
+    };
 
     return(
         <div className={s.postsBlock}>
             <h3>My posts</h3>
             <div>
                 <div>
-                    <textarea cols="30" rows="10"></textarea>
+                    <textarea cols="30" rows="10" ref={newPostElement}></textarea>
                 </div>
                 <div>
-                    <button>Add post</button>
+                    <button onClick={ addPost }>Add post</button>
                 </div>
             </div>
             <div className={s.posts}>
