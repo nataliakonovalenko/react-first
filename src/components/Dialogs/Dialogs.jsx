074b1235ff433de5b206525ae0d79adebe7f8af8
Natalia_Konovalenko <natalia_konovalenko@epam.com>
2020-02-10 16:30:07 +0200

    Redirect to Login page

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import s from './Dialogs.module.css';
 import DialogItem from "./DialogItem/DialogItem";
 import Message from "./Message/Message";
+import {Redirect} from "react-router-dom";
 
 const Dialogs = (props) => {
     let dialogsElements = props.dialogs.map( dialog => <DialogItem name={dialog.name} key={dialog.id} id={dialog.id} />);
@@ -15,6 +16,8 @@ const Dialogs = (props) => {
         let text = e.target.value;
         props.updateNewMessage(text);
     };
+
+    if (!props.isAuth) return <Redirect to={'/login'} />;
 
     return (
         <div className={s.dialogs}>
