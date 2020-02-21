8ae38422bce26a957c191eddc488ad0e65a49d09
Natalia_Konovalenko <natalia_konovalenko@epam.com>
2020-02-21 11:54:11 +0200

    useState / useEffect

--- src/components/Profile/ProfileInfo/ProfileInfo.jsx
+++ src/components/Profile/ProfileInfo/ProfileInfo.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import s from './ProfileInfo.module.css';
 import Preloader from "../../Preloader/Preloader";
 import ProfileStatus from "./ProfileStatus";
-import {updateStatus} from "../../../redux/profile-reducer";
+import ProfileStatusWithHooks from "./ProfileStatusWithHooks";
 
 const ProfileInfo  = (props) => {
     if (!props.profile) {
@@ -13,7 +13,7 @@ const ProfileInfo  = (props) => {
         <div>
             <div className={s.descriptonBlock}>
                 <img src={props.profile.photos.large} alt=""/>
-                <ProfileStatus status={props.status} updateStatus={props.updateStatus}/>
+                <ProfileStatusWithHooks status={props.status} updateStatus={props.updateStatus}/>
                 {/*<div>Name: {props.profile.fullName}</div>
                 <div>About: {props.profile.aboutMe}</div>
                 <div>Looking for a job: {props.profile.lookingForAJob === true ? 'Yes' : 'No'}</div>
